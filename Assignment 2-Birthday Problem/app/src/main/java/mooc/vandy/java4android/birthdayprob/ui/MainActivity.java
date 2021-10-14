ef0ec28c9568e36cca15a2da4eedfff00f4bd62a
Vidish Naik <vidishnaik0509@gmail.com>
2021-10-13 22:50:21 -0700

    Bump gradle version

--- Assignment 2-Birthday Problem/app/src/main/java/mooc/vandy/java4android/birthdayprob/ui/MainActivity.java
+++ Assignment 2-Birthday Problem/app/src/main/java/mooc/vandy/java4android/birthdayprob/ui/MainActivity.java
@@ -1,6 +1,6 @@
 package mooc.vandy.java4android.birthdayprob.ui;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
